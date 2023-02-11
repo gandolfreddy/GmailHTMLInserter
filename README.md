c0c1cd587a79780a55b6e086bffe70ad79b8e8cd
Freddy <freddy.wei0810@gmail.com>
2023-02-11 20:26:25 +0800

    feat: update README.md & test auto reinstall via link

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A simple Gmail HTML inserter
 
 ## 測試更新
 
-[連結](https://github.com/gandolfreddy/GmailHTMLInserter/raw/main/src/GmailHTMLInserter.js)
+[連結](https://github.com/gandolfreddy/GmailHTMLInserter/raw/main/src/GmailHTMLInserter.user.js)
--- src/GmailHTMLInserter.user.js
+++ src/GmailHTMLInserter.user.js
@@ -3,156 +3,154 @@
 // @version      0.2.0
 // @description  A simple Gmail HTML inserter
 // @license      GPL
-// @updateURL    https://github.com/gandolfreddy/GmailHTMLInserter/raw/main/src/GmailHTMLInserter.js
-// @downloadURL  https://github.com/gandolfreddy/GmailHTMLInserter/raw/main/src/GmailHTMLInserter.js
 // @source       https://github.com/gandolfreddy/GmailHTMLInserter/raw/main/src/GmailHTMLInserter.js
 // @namespace    https://github.com/gandolfreddy/GmailHTMLInserter/raw/main/src/GmailHTMLInserter.js
-// @author       Gandolfreddy
 // @match        https://mail.google.com/mail/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hibbard.eu
-// @run-at       document-start
+// @author       Gandolfreddy
+// @run-at       document-idle
 // ==/UserScript==
 
 
-const styleSheet = `
-/* 自定義 CSS */
-.html-inserter-btn {
-    color: white;
-    background-color: #219d85;
-    border: 0px;
-    border-radius: 5px;
-    box-shadow: 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-.html-inserter-btn:hover{
-    background-color: #18bc9c;
-    cursor:pointer;
-}
-
-.html-inserter-mask {
-    visibility: hidden;
-
-    background-color: rgba(0, 0, 0, 0.5);
-    height: 100%;
-    width: 100%;
-    z-index: 7;
-
-    position: absolute;
-    top: 0;
-    left: 0;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.html-inserter-editor {
-    width: 800px;
-    padding-buttom: 10px;
-
-    background-color: #ffffff;
-    position: absolute;
-    border-radius: 5px;
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-.html-inserter-editor-title {
-    width: 100%;
-    height: 35px;
-    margin-bottom: 20px;
-
-    background-color: #f2f6fc;
-    border-radius: 5px 5px 0px 0px;
-
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-}
-
-.close-btn {
-    border: 0px;
-    border-radius: 2px;
-    margin-right: 10px;
-
-    font-size: 20px;
-    font-family: consolas;
-    color: #878a8b;
-    background-color: #f2f6fc;
-}
-
-.close-btn:hover{
-    background-color: #c1c1c1;
-    cursor:pointer;
-}
-
-.html-inserter-editor-container {
-    width: 95%;
-    height: 600px;
-    margin-bottom: 25px;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.html-content {
-    width: 100%;
-    height: 100%;
-    resize: none;
-
-    border: 2px #ececec solid;
-    border-radius: 3px;
-    padding: 10px;
-
-    font-size: 16px;
-    font-family: consolas;
-}
-
-.html-content:focus {
-    outline: none !important;
-    border-color: #c1c1c1;
-}
-
-.html-inserter-editor-footer {
-    width: 95%;
-    margin-bottom: 15px;
-
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-}
-
-.send-btn {
-    width: 100px;
-    height: 30px;
-
-    font-size: 15px;
-    font-family: consolas;
-
-    color: white;
-    background-color: #219d85;
-    border: 0px;
-    border-radius: 3px;
-    box-shadow: 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-.send-btn:hover{
-    background-color: #18bc9c;
-    cursor:pointer;
-}
-`;
-
-const s = document.createElement('style');
-s.type = 'text/css';
-s.innerHTML = styleSheet;
-(document.head || document.documentElement).appendChild(s);
-
-
-window.addEventListener("load", function (event) {
+(function () {
     'use strict';
+
+    const styleSheet = `
+    /* 自定義 CSS */
+    .html-inserter-btn {
+        color: white;
+        background-color: #219d85;
+        border: 0px;
+        border-radius: 5px;
+        box-shadow: 2px 2px rgba(0, 0, 0, 0.2);
+    }
+
+    .html-inserter-btn:hover{
+        background-color: #18bc9c;
+        cursor:pointer;
+    }
+
+    .html-inserter-mask {
+        visibility: hidden;
+
+        background-color: rgba(0, 0, 0, 0.5);
+        height: 100%;
+        width: 100%;
+        z-index: 7;
+
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .html-inserter-editor {
+        width: 800px;
+        padding-buttom: 10px;
+
+        background-color: #ffffff;
+        position: absolute;
+        border-radius: 5px;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .html-inserter-editor-title {
+        width: 100%;
+        height: 35px;
+        margin-bottom: 20px;
+
+        background-color: #f2f6fc;
+        border-radius: 5px 5px 0px 0px;
+
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+
+    .close-btn {
+        border: 0px;
+        border-radius: 2px;
+        margin-right: 10px;
+
+        font-size: 20px;
+        font-family: consolas;
+        color: #878a8b;
+        background-color: #f2f6fc;
+    }
+
+    .close-btn:hover{
+        background-color: #c1c1c1;
+        cursor:pointer;
+    }
+
+    .html-inserter-editor-container {
+        width: 95%;
+        height: 600px;
+        margin-bottom: 25px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .html-content {
+        width: 100%;
+        height: 100%;
+        resize: none;
+
+        border: 2px #ececec solid;
+        border-radius: 3px;
+        padding: 10px;
+
+        font-size: 16px;
+        font-family: consolas;
+    }
+
+    .html-content:focus {
+        outline: none !important;
+        border-color: #c1c1c1;
+    }
+
+    .html-inserter-editor-footer {
+        width: 95%;
+        margin-bottom: 15px;
+
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    .send-btn {
+        width: 100px;
+        height: 30px;
+
+        font-size: 15px;
+        font-family: consolas;
+
+        color: white;
+        background-color: #219d85;
+        border: 0px;
+        border-radius: 3px;
+        box-shadow: 2px 2px rgba(0, 0, 0, 0.2);
+    }
+
+    .send-btn:hover{
+        background-color: #18bc9c;
+        cursor:pointer;
+    }
+    `;
+
+    const s = document.createElement('style');
+    s.type = 'text/css';
+    s.innerHTML = styleSheet;
+    (document.head || document.documentElement).appendChild(s);
+
 
     // Options for the observer (which mutations to observe)
     const config = { attributes: true, childList: true, subtree: true };
@@ -262,4 +260,4 @@ window.addEventListener("load", function (event) {
         // Start observing the target node for configured mutations
         bodyObserver.observe(body, config);
     }
-});
+})();
